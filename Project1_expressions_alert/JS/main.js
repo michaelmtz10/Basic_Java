99079813593cc807ed876d89d4bfbe986caaadcb
michaelmtz10 <85362903+michaelmtz10@users.noreply.github.com>
2021-06-28 11:24:23 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,42 @@
+
+document.write('Im Papa'); //displaying the string 'Im papa'
+
+
+var c = ' and This is the end'; // assigning a variable a string
+
+document.write(c);
+
+eval('distance = 345'); // assigning a string to a variable, the variable distance was assigned the string 345
+console.log(distance);
+
+var myfirstname = ('Michael'); //creating a variable and assigning it a string
+var myfirstname = myfirstname.fontcolor('Red') // giving font color to my string
+document.write(myfirstname);
+var mylastname = ('martinez'); //creating a variable and assigning it a string
+var mylastname = mylastname.fontcolor("Green"); // giving font color to my string
+document.write(mylastname);
+
+alert(myfirstname);
+
+document.write(' and I said to my friend \"what should we do later?');
+
+document.write(' very few have heard the saying ' + 'can a sea horse, see a horse ' + 'in a sea full of horses?');
+
+var x = (' this america' + ' they want nothing to do with us'); //creating a variable and assigning it a concatenated string
+var x = x.fontcolor("Blue"); // giving font color to my string
+document.write(x);
+
+var fam = dad =('pedro'), mom = ('jessica'), son =('lamar');
+var fam = fam.fontcolor("Orange"); // giving font color to my string
+document.write(mom);
+
+var car = " the old car"; //creating a variable and assigning it a string
+var car = car.fontcolor("orange");
+document.write(car);
+
+document.write(6+6);
+
+function myfunction(){
+    var m = document.getElementById("fname");
+    x.value = x.value.toUppercase();
+}
